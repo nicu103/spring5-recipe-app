445c1c9f21ae740fc506fc20cbcaa3fffd0f56d5
nicu <no@mail.com>
2020-08-02 21:39:27 +0300

    Uses Spring Developer Tools

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -8,6 +8,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        System.out.println("Something to say! saY saY");
         return "index";
     }
 }
