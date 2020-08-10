a68abba6a4f25abd7184ccaf42b93447b7a3b83e
nicu <no@mail.com>
2020-08-10 23:46:26 +0300

    Adds lombok and refactors Category, Recipe and the Recipe Service

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -1,9 +1,12 @@
 package guru.springframework.domain;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Data
 @Entity
 public class Category {
     @Id
@@ -15,27 +18,11 @@ public class Category {
     @ManyToMany(mappedBy = "categories")
     private Set<Recipe> recipes = new HashSet<>();
 
-    public Long getId() {
-        return id;
+    public Category() {
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    protected boolean canEqual(final Object other) {
+        return other instanceof Category;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Set<Recipe> getRecipes() {
-        return recipes;
-    }
-
-    public void setRecipes(Set<Recipe> recipes) {
-        this.recipes = recipes;
-    }
 }
